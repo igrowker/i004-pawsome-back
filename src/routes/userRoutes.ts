79143aa2adf7377db6e6fb5327ca994d018824d4
Maximiliano Salguero <salgueromaximiliano8@gmail.com>
2024-11-21 21:05:56 -0300

    Endpoint update user - Done

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUsers, getUserById } from '../controllers/userController';
+import { getUsers, getUserById, updateUser } from '../controllers/userController';
 
 const userRoutes = express.Router();
 
 userRoutes.get('/', getUsers);
 userRoutes.get('/:id', getUserById);
+userRoutes.put('/:id', updateUser);
 
 export default userRoutes;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,5 @@
 import Usuario from '../models/userModel';
+import mongoose from 'mongoose';
 
 export const getUsersService = async () => {
     const users = await Usuario.find().select('-password'); // Excluye el campo password
@@ -11,4 +12,18 @@ export const getUserByIdService = async (id: string) => {
         throw new Error('Usuario no encontrado');
     }
     return user;
+};
+
+export const updateUserService = async (id: string, updateData: any) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new Error('ID de usuario no válido');
+    }
+
+    const updatedUser = await Usuario.findByIdAndUpdate(id, updateData, { new: true });
+
+    if (!updatedUser) {
+        throw new Error('Usuario no encontrado');
+    }
+
+    return updatedUser;
 };
