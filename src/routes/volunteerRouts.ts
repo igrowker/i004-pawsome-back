c15e88d6332a85d075ac0443ef728af6e7eda572
AJPadillo <ajpadillo@gmail.com>
2024-11-26 12:24:21 +0100

    agregada logica y endpoint de eliminacion de oportunidad de voluntariado

--- src/routes/volunteerRouts.ts
+++ src/routes/volunteerRouts.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createVolunteerController, getVolunteerOpportunities, getVolunteerOpportunitiesByRefugeeId } from '../controllers/volunteersController';
+import { createVolunteerController, getVolunteerOpportunities, getVolunteerOpportunitiesByRefugeeId, deleteVolunteerOpportunity } from '../controllers/volunteersController';
 
 const volunteerRoutes = express.Router();
 
 volunteerRoutes.get('/', getVolunteerOpportunities);
 volunteerRoutes.get('/:id', getVolunteerOpportunitiesByRefugeeId);
 volunteerRoutes.post('/:id',createVolunteerController);
+volunteerRoutes.delete('/:id', deleteVolunteerOpportunity);
+
 
 
 export default volunteerRoutes;
