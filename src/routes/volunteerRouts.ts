619b85ee8e8b66878b0dac96d987832f9b49fc9a
fhuayller <fhuayller@gmail.com>
2024-11-28 10:12:00 -0300

    authToken a getVolunteers

--- src/routes/volunteerRouts.ts
+++ src/routes/volunteerRouts.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createVolunteerController, getVolunteerOpportunities, getVolunteerOpportunitiesByRefugeeId, deleteVolunteerOpportunity } from '../controllers/volunteersController';
+import { authenticateToken } from '../middlewares/authMiddleware';
 
 const volunteerRoutes = express.Router();
 
-volunteerRoutes.get('/', getVolunteerOpportunities);
+volunteerRoutes.get('/', authenticateToken,getVolunteerOpportunities);
 volunteerRoutes.get('/:id', getVolunteerOpportunitiesByRefugeeId);
 volunteerRoutes.post('/:id',createVolunteerController);
 volunteerRoutes.delete('/:id', deleteVolunteerOpportunity);
