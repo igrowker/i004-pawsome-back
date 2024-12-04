942c37acd4eecd0e7720d1c0e9cc83fb944f9c9b
fhuayller <fhuayller@gmail.com>
2024-12-04 14:13:26 -0300

    volunteerModel se relaciona con el usuario refugio que crea esa oportunidad

--- src/routes/volunteerRouts.ts
+++ src/routes/volunteerRouts.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { createVolunteerController, getVolunteerOpportunities, getVolunteerOpportunitiesByRefugeeId, deleteVolunteerOpportunity, updateVolunteerOpportunity } from '../controllers/volunteersController';
 import { checkRole } from '../middlewares/roleMiddleware';
-import { authenticateToken } from '../middlewares/authMiddleware';
 
 const volunteerRoutes = express.Router();
 
-volunteerRoutes.get('/', authenticateToken,getVolunteerOpportunities);
-volunteerRoutes.get('/:id', getVolunteerOpportunitiesByRefugeeId);
+volunteerRoutes.get('/',getVolunteerOpportunities);
+volunteerRoutes.get('/:refugeeId', getVolunteerOpportunitiesByRefugeeId);
 volunteerRoutes.post('/:id', checkRole('refugee'), createVolunteerController);
 volunteerRoutes.delete('/:id', checkRole('refugee'), deleteVolunteerOpportunity);
 volunteerRoutes.put('/:opportunityId', updateVolunteerOpportunity);
